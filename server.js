9e027df13636dce5eec35cc907db9190491535e6
mattdean <matthewallandean@gmail.com>
2021-05-08 10:48:14 -0600

    Connect schema file

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const express = require('express')
+const schema = require('./schema');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
