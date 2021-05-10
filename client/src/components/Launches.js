cbd7e34a6f2ef3ab920d04b1654870db36737e35
mattdean <matthewallandean@gmail.com>
2021-05-10 09:01:11 -0600

    Allow cors and retrieve data

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -20,7 +20,11 @@ export default function Launches() {
       <Query query={LAUNCHES_QUERY}>
         {
           ({ loading, error, data }) => {
-            
+            if (loading) return <h4>Loading...</h4>
+            if (error) console.log(error);
+            console.log(data);
+
+            return <h1>test</h1>
           }
         }
       </Query>
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Middleware
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
